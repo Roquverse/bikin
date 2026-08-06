359c39c701b8b874a36a6f886a7feafdedc32877
Oluwadamilola Cole <arakunrincole@Oluwadamilolas-MacBook-Pro.local>
2026-08-07 00:19:26 +0100

    fix: import Role from @prisma/client in users.service.ts

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../common/prisma/prisma.service';
-import { Prisma, User } from '@prisma/client';
+import { Prisma, Role, User } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
